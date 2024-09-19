433dbe25f107905253019fb6b31c2e06af8b568b
Andrew <andrewqp03@gmail.com>
2024-09-19 17:10:36 -0500

    Checkoutpopup almost finished. Need to connect to database when confirming order.

--- src/components/CheckoutPopup.js
+++ src/components/CheckoutPopup.js
@@ -6,51 +6,110 @@ const CheckoutPopup = ({ onClose, subtotal }) => {
   const [total, setTotal] = useState(0.0);
   const [tableNumber, setTableNumber] = useState(0);
   const [receivedAmount, setReceivedAmount] = useState(0.00);
+  const [tipPercent, setTipPercent] = useState(0);
+  const [tipAmount, setTipAmount] = useState(0.00);
+  const [changeAmount, setChangeAmount] = useState(0.00);
+  const [error, setError] = useState("");
+  const [confirmOrderButton, setConfrimOrderButton] = useState("Confrim Order");
 
-  useEffect(() => {
-    const taxRate = 0.0825; // 8.25% tax rate
+  useEffect(() => { // Handle all of the calculations
     const numericSubtotal = parseFloat(subtotal); // Treat as number, not string
+
+    const taxRate = 0.0825; // 8.25% tax rate
     const calculatedTax = numericSubtotal * taxRate;
     setTax(calculatedTax.toFixed(2));
-    const total = calculatedTax + numericSubtotal;
+
+    const calculateTip = numericSubtotal * (tipPercent/100);
+    setTipAmount(calculateTip.toFixed(2))
+
+    const total = calculateTip + calculatedTax + numericSubtotal;
     setTotal(total.toFixed(2));
-  }, [subtotal]);
+
+    const calculateChange = receivedAmount - total;
+    setChangeAmount(calculateChange.toFixed(2))
+  }, [subtotal, tipPercent, receivedAmount]);
+
+  const handleConfirmOrder = (e) => {
+    e.preventDefault();
+    if (changeAmount < 0) {
+      const neededMoney = (changeAmount * -1).toFixed(2);
+      setError(`Insufficient Funds. Need $${neededMoney} more`);
+    }
+    else if (tableNumber === 0) {
+      setError("Invalid table number")
+    }
+    else {
+      setError("");
+      setConfrimOrderButton("Loading...");
+      // send to server
+      setConfrimOrderButton("Confirm Order");
+    }
+  }
 
   return (
     <div className="checkout-popup-overlay">
       <div className="checkout-popup-content">
-        <button onClick={onClose}>Close</button>
-        <h1>Payment</h1>
-        <form>
-          <div>
+        <div className="checkout-header">
+          <h1>Payment</h1>
+          <button onClick={onClose}>Close</button>
+        </div>
+        <form onSubmit={handleConfirmOrder}>
+          <div className="checkout-label">
             <label>Table number: </label>
             <input
               type="number"
               value={tableNumber}
               onChange={(e) => setTableNumber(e.target.value)}
               min="0"
-              placeholder="Enter table number"
+              placeholder="1"
+              required
             />
           </div>
-          <div>
-            <label>Subtotal: ${subtotal}</label>
+          <div className="checkout-label">
+            <label>Subtotal: </label>
+            <p>${subtotal}</p>
           </div>
-          <div>
-            <label>Tax: ${tax}</label>
+          <div className="checkout-label">
+            <label>Tax: </label>
+            <p>${tax}</p>
           </div>
-          <div>
-            <label>Total: ${total}</label>
+          <div className="checkout-label">
+            <label>Tip {tipPercent}%: </label>
+            <p>${tipAmount}</p>
           </div>
-          <div>
+          <div className="range-container">
+            <input
+              type="range"
+              min="0"
+              max="25"
+              step="5"
+              value={tipPercent}
+              onChange={(e) => setTipPercent(e.target.value)}
+              required
+            />
+          </div>
+          <div className="checkout-label">
+            <label>Total: </label>
+            <p>${total}</p>
+          </div>
+          <div className="checkout-label">
             <label>Received Amount: $</label>
             <input
               type="number"
               value={receivedAmount}
               onChange={(e) => setReceivedAmount(e.target.value)}
               min="0"
-              placeholder="Enter amount received"
+              placeholder="0.00"
             />
           </div>
+          {changeAmount > 0 &&
+            <div className="checkout-label">
+              <label>Change: </label>
+              <p>${changeAmount}</p>
+            </div>
+          }
+          {error && <p className="confirm-order-error">{error}</p>}
+          <button className="confirm-order-button">{confirmOrderButton}</button>
         </form>
       </div>
     </div>
