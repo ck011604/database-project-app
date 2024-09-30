73f18c703555fe7dcecf80a196a4bfc78067b5e6
ck011604 <ck011604@gmail.com>
2024-09-30 16:50:06 -0500

    added loyalty program tables

--- restaurantDB.sql
+++ restaurantDB.sql
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -38,9 +38,11 @@ CREATE TABLE `employees` (
 -- Dumping data for table `employees`
 --
 
+LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
 INSERT INTO `employees` VALUES (1,'Amy','Lee','amy@example.com','Manager',1,'123'),(2,'Bob','Ross','bob@example.com','Waiter',0,'123'),(3,'Sam','Do','sam@example.com','Chef',0,'123'),(4,'Taylor','Swift','taylor@example.com','Manager',1,'123'),(5,'Dan','Pie','dan@example.com','Chef',0,'123'),(6,'Daz','Pir','daz@example.com','Chef',0,'123'),(7,'Daz','Pir','daz@example.com','Chef',0,'123');
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `inventory`
@@ -63,9 +65,11 @@ CREATE TABLE `inventory` (
 -- Dumping data for table `inventory`
 --
 
+LOCK TABLES `inventory` WRITE;
 /*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
 INSERT INTO `inventory` VALUES (1,'mozzarella',75,20,100),(2,'tomato sauce',95,10,100),(3,'dough',95,10,50),(4,'pepperoni',100,50,100),(5,'cheddar cheese',100,20,100),(6,'beef patty',100,20,50),(7,'lettuce',100,20,50),(8,'tomato',100,20,50),(9,'pasta',100,20,50),(10,'chicken',100,20,70),(11,'garlic',100,20,50),(12,'fish fillets',70,10,30),(13,'tortillas',100,20,50),(14,'cabbage',100,20,50),(15,'lime',100,40,50),(16,'sour cream',50,20,50),(17,'milk',100,20,50),(18,'butter',100,30,100),(19,'flour',100,40,200),(20,'hamburger bun',100,20,75),(21,'hot dog sausage',100,20,50),(22,'hot dog bun',100,30,100),(23,'mustard',100,40,100),(24,'ketchup',100,30,100),(25,'relish',50,20,45),(26,'potatoes',100,30,100),(27,'carrots',100,20,60),(28,'mayonnaise',70,10,40),(29,'bread',100,10,40),(30,'corn',100,10,50),(31,'salt',100,40,100),(32,'Sprite',97,20,100),(33,'Coca-Cola',100,20,100),(34,'Pepsi',100,20,100),(35,'Dr. Pepper',100,20,100),(36,'Fanta Orange',0,20,100),(37,'Diet Coke',93,20,100),(38,'Black Tea',100,20,70),(39,'Sugar',100,20,100);
 /*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `inventory_logs`
@@ -90,8 +94,10 @@ CREATE TABLE `inventory_logs` (
 -- Dumping data for table `inventory_logs`
 --
 
+LOCK TABLES `inventory_logs` WRITE;
 /*!40000 ALTER TABLE `inventory_logs` DISABLE KEYS */;
 /*!40000 ALTER TABLE `inventory_logs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `menu`
@@ -116,9 +122,11 @@ CREATE TABLE `menu` (
 -- Dumping data for table `menu`
 --
 
+LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
 INSERT INTO `menu` VALUES (1,'Cheese Pizza','[{\"quantity\": 1, \"ingredient_id\": 2}, {\"quantity\": 5, \"ingredient_id\": 1}, {\"quantity\": 1, \"ingredient_id\": 3}]',15.99,'cheese_pizza.jpg','main'),(2,'Pepperoni Pizza','[{\"quantity\": 1, \"ingredient_id\": 2}, {\"quantity\": 5, \"ingredient_id\": 1}, {\"quantity\": 1, \"ingredient_id\": 3}, {\"quantity\": 20, \"ingredient_id\": 4}]',15.99,'pepperoni_pizza.jpg','main'),(3,'Cheese Burger','[{\"quantity\": 2, \"ingredient_id\": 5}, {\"quantity\": 1, \"ingredient_id\": 6}, {\"quantity\": 2, \"ingredient_id\": 7}, {\"quantity\": 2, \"ingredient_id\": 8}, {\"quantity\": 1, \"ingredient_id\": 20}]',7.99,'cheese_burger.jpg','main'),(4,'Chicken Alfredo','[{\"quantity\": 10, \"ingredient_id\": 9}, {\"quantity\": 2, \"ingredient_id\": 10}, {\"quantity\": 3, \"ingredient_id\": 11}]',10.99,'chicken_alfredo.jpg','main'),(5,'Fish Tacos','[{\"quantity\": 3, \"ingredient_id\": 12}, {\"quantity\": 5, \"ingredient_id\": 13}, {\"quantity\": 3, \"ingredient_id\": 14}, {\"quantity\": 3, \"ingredient_id\": 15}, {\"quantity\": 4, \"ingredient_id\": 16}]',8.99,'fish_tacos.jpg','main'),(6,'Mac and Cheese','[{\"quantity\": 7, \"ingredient_id\": 9}, {\"quantity\": 10, \"ingredient_id\": 5}, {\"quantity\": 3, \"ingredient_id\": 17}, {\"quantity\": 5, \"ingredient_id\": 18}, {\"quantity\": 3, \"ingredient_id\": 19}]',4.99,'mac_and_cheese.jpg','side'),(7,'Hot Dogs','[{\"quantity\": 3, \"ingredient_id\": 21}, {\"quantity\": 3, \"ingredient_id\": 22}, {\"quantity\": 3, \"ingredient_id\": 23}, {\"quantity\": 3, \"ingredient_id\": 24}, {\"quantity\": 5, \"ingredient_id\": 25}]',8.99,'hot_dogs.jpg','main'),(8,'Mashed Potatoes','[{\"quantity\": 1, \"ingredient_id\": 26}, {\"quantity\": 3, \"ingredient_id\": 18}, {\"quantity\": 1, \"ingredient_id\": 17}, {\"quantity\": 5, \"ingredient_id\": 31}]',4.99,'mashed_potatoes.jpg','side'),(9,'Coleslaw','[{\"quantity\": 1, \"ingredient_id\": 14}, {\"quantity\": 1, \"ingredient_id\": 27}, {\"quantity\": 2, \"ingredient_id\": 28}]',3.99,'coleslaw.jpg','side'),(10,'Garlic Bread','[{\"quantity\": 3, \"ingredient_id\": 29}, {\"quantity\": 5, \"ingredient_id\": 18}, {\"quantity\": 3, \"ingredient_id\": 11}]',4.99,'garlic_bread.jpg','side'),(11,'Corn','[{\"quantity\": 2, \"ingredient_id\": 30}, {\"quantity\": 3, \"ingredient_id\": 31}, {\"quantity\": 5, \"ingredient_id\": 18}]',3.99,'corn.jpg','side'),(12,'Sprite','[{\"quantity\": 1, \"ingredient_id\": 32}]',1.99,'sprite.png','drink'),(13,'Coca-Cola','[{\"quantity\": 1, \"ingredient_id\": 33}]',1.99,'coca-cola.png','drink'),(14,'Pepsi','[{\"quantity\": 1, \"ingredient_id\": 34}]',1.99,'pepsi.png','drink'),(15,'Dr. Pepper','[{\"quantity\": 1, \"ingredient_id\": 35}]',1.99,'dr-pepper.png','drink'),(16,'Fanta Orange','[{\"quantity\": 1, \"ingredient_id\": 36}]',1.99,'fanta.png','drink'),(17,'Diet Coke','[{\"quantity\": 1, \"ingredient_id\": 37}]',1.99,'diet_coke.png','drink'),(18,'Sweet Tea','[{\"quantity\": 1, \"ingredient_id\": 38}, {\"quantity\": 5, \"ingredient_id\": 39}]',1.99,'sweet_tea.jpg','drink');
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `orders`
@@ -158,38 +166,41 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
+LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
 INSERT INTO `orders` VALUES (1,'[{\"recipe_id\":1,\"name\":\"Cheese Pizza\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":2},{\"quantity\":5,\"ingredient_id\":1},{\"quantity\":1,\"ingredient_id\":3}],\"price\":\"15.99\",\"image\":\"cheese_pizza.jpg\",\"type\":\"main\",\"quantity\":3}]',1,1,1,47.97,25,11.99,63.92,64.00,0.08,3.96,'In-Queue','2024-09-29 23:02:50','This is a test for special requests.'),(2,'[{\"recipe_id\":1,\"name\":\"Cheese Pizza\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":2},{\"quantity\":5,\"ingredient_id\":1},{\"quantity\":1,\"ingredient_id\":3}],\"price\":\"15.99\",\"image\":\"cheese_pizza.jpg\",\"type\":\"main\",\"quantity\":10}]',1,1,1,159.90,20,31.98,205.07,206.00,0.93,13.19,'In-Queue','2024-09-29 23:07:32',''),(3,'[{\"recipe_id\":15,\"name\":\"Dr. Pepper\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":35}],\"price\":\"1.99\",\"image\":\"dr-pepper.png\",\"type\":\"drink\",\"quantity\":5}]',1,1,1,9.95,0,0.00,10.77,11.00,0.23,0.82,'In-Queue','2024-09-29 23:09:53',''),(4,'[{\"recipe_id\":15,\"name\":\"Dr. Pepper\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":35}],\"price\":\"1.99\",\"image\":\"dr-pepper.png\",\"type\":\"drink\",\"quantity\":5}]',1,1,1,9.95,0,0.00,10.77,11.00,0.23,0.82,'In-Queue','2024-09-30 02:55:15',''),(5,'[{\"recipe_id\":15,\"name\":\"Dr. Pepper\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":35}],\"price\":\"1.99\",\"image\":\"dr-pepper.png\",\"type\":\"drink\",\"quantity\":5}]',1,1,1,9.95,0,0.00,10.77,11.00,0.23,0.82,'In-Queue','2024-09-30 03:06:34',''),(6,'[{\"recipe_id\":18,\"name\":\"Sweet Tea\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":38},{\"quantity\":5,\"ingredient_id\":39}],\"price\":\"1.99\",\"image\":\"sweet_tea.jpg\",\"type\":\"drink\",\"quantity\":5}]',1,1,1,9.95,15,1.49,12.26,15.00,2.74,0.82,'In-Queue','2024-09-30 03:15:09','Add less sugar'),(7,'[{\"recipe_id\":2,\"name\":\"Pepperoni Pizza\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":2},{\"quantity\":5,\"ingredient_id\":1},{\"quantity\":1,\"ingredient_id\":3},{\"quantity\":20,\"ingredient_id\":4}],\"price\":\"15.99\",\"image\":\"pepperoni_pizza.jpg\",\"type\":\"main\",\"quantity\":5}]',1,1,1,79.95,15,11.99,98.54,100.00,1.46,6.60,'In-Queue','2024-09-30 03:28:49','Double pepperoni'),(8,'[{\"recipe_id\":12,\"name\":\"Sprite\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":32}],\"price\":\"1.99\",\"image\":\"sprite.png\",\"type\":\"drink\",\"quantity\":1}]',1,1,NULL,1.99,5,0.10,2.25,5.00,2.75,0.16,'In-Queue','2024-09-30 04:37:36',''),(9,'[{\"recipe_id\":12,\"name\":\"Sprite\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":32}],\"price\":\"1.99\",\"image\":\"sprite.png\",\"type\":\"drink\",\"quantity\":2}]',1,1,NULL,3.98,0,0.00,4.31,5.00,0.69,0.33,'In-Queue','2024-09-30 05:02:47',''),(10,'[{\"recipe_id\":16,\"name\":\"Fanta Orange\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":36}],\"price\":\"1.99\",\"image\":\"fanta.png\",\"type\":\"drink\",\"quantity\":1}]',1,1,NULL,1.99,0,0.00,2.15,3.00,0.85,0.16,'In-Queue','2024-09-30 05:06:36',''),(11,'[{\"recipe_id\":17,\"name\":\"Diet Coke\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":37}],\"price\":\"1.99\",\"image\":\"diet_coke.png\",\"type\":\"drink\",\"quantity\":1}]',1,1,NULL,1.99,0,0.00,2.15,3.00,0.85,0.16,'In-Queue','2024-09-30 05:08:57',''),(12,'[{\"recipe_id\":17,\"name\":\"Diet Coke\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":37}],\"price\":\"1.99\",\"image\":\"diet_coke.png\",\"type\":\"drink\",\"quantity\":1}]',1,1,2,1.99,0,0.00,2.15,3.00,0.85,0.16,'In-Queue','2024-09-30 05:16:18',''),(13,'[{\"recipe_id\":17,\"name\":\"Diet Coke\",\"ingredients\":[{\"quantity\":1,\"ingredient_id\":37}],\"price\":\"1.99\",\"image\":\"diet_coke.png\",\"type\":\"drink\",\"quantity\":5}]',1,1,NULL,9.95,0,0.00,10.77,11.00,0.23,0.82,'In-Queue','2024-09-30 05:21:05','');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `request_schedule`
+-- Table structure for table `redemptions`
 --
 
-DROP TABLE IF EXISTS `request_schedule`;
+DROP TABLE IF EXISTS `redemptions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `request_schedule` (
-  `request_id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
-  `employee_id` int unsigned NOT NULL,
-  `request_type` varchar(50) NOT NULL,
-  `request_start_date` date NOT NULL,
-  `request_end_date` date NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `submitted_at` datetime NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`request_id`),
-  KEY `fk_request_schedule_employee_id` (`employee_id`),
-  CONSTRAINT `fk_request_schedule_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='request schedule change';
+CREATE TABLE `redemptions` (
+  `redemption_id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `user_id` int unsigned NOT NULL,
+  `reward_id` int unsigned NOT NULL,
+  `redemption_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `is_active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`redemption_id`),
+  KEY `fk_user_redemption` (`user_id`),
+  KEY `fk_reward_redemption` (`reward_id`),
+  CONSTRAINT `fk_reward_redemption` FOREIGN KEY (`reward_id`) REFERENCES `rewards` (`reward_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_user_redemption` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `request_schedule`
+-- Dumping data for table `redemptions`
 --
 
-/*!40000 ALTER TABLE `request_schedule` DISABLE KEYS */;
-/*!40000 ALTER TABLE `request_schedule` ENABLE KEYS */;
+LOCK TABLES `redemptions` WRITE;
+/*!40000 ALTER TABLE `redemptions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `redemptions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `restaurant_tables`
@@ -216,37 +227,37 @@ CREATE TABLE `restaurant_tables` (
 -- Dumping data for table `restaurant_tables`
 --
 
+LOCK TABLES `restaurant_tables` WRITE;
 /*!40000 ALTER TABLE `restaurant_tables` DISABLE KEYS */;
 INSERT INTO `restaurant_tables` VALUES (1,NULL,NULL,'Free');
 /*!40000 ALTER TABLE `restaurant_tables` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `schedule`
+-- Table structure for table `rewards`
 --
 
-DROP TABLE IF EXISTS `schedule`;
+DROP TABLE IF EXISTS `rewards`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `schedule` (
-  `schedule_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
-  `employee_id` int unsigned NOT NULL,
-  `shift_id` int unsigned NOT NULL,
-  `schedule_date` date NOT NULL,
-  `is_active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`schedule_id`),
-  KEY `fk_schedule_employee_id` (`employee_id`),
-  KEY `fk_schedule_shift_id` (`shift_id`),
-  CONSTRAINT `fk_schedule_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_schedule_shift_id` FOREIGN KEY (`shift_id`) REFERENCES `shifts` (`shift_id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `rewards` (
+  `reward_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `reward_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `points_required` int unsigned NOT NULL,
+  `reward_description` text,
+  `is_active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`reward_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `schedule`
+-- Dumping data for table `rewards`
 --
 
-/*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
-/*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
+LOCK TABLES `rewards` WRITE;
+/*!40000 ALTER TABLE `rewards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rewards` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `shifts`
@@ -256,13 +267,14 @@ DROP TABLE IF EXISTS `shifts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `shifts` (
-  `shift_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
-  `shift_date` date NOT NULL,
-  `shift_start_time` time NOT NULL,
-  `shift_end_time` time NOT NULL,
-  `shift_name` varchar(50) NOT NULL,
-  `is_filled` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`shift_id`)
+  `shift_id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `employee_id` int unsigned DEFAULT NULL,
+  `shift_start_time` datetime DEFAULT NULL,
+  `shift_end_time` datetime DEFAULT NULL,
+  `is_filled` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`shift_id`),
+  KEY `employee_id` (`employee_id`),
+  CONSTRAINT `shifts_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -270,8 +282,10 @@ CREATE TABLE `shifts` (
 -- Dumping data for table `shifts`
 --
 
+LOCK TABLES `shifts` WRITE;
 /*!40000 ALTER TABLE `shifts` DISABLE KEYS */;
 /*!40000 ALTER TABLE `shifts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -297,92 +311,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
+LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'Mike','Ross','mikeross@gmail.com','mike123','user',1,0),(2,'Tony','Smith','ts@example.com','ts123','user',1,0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
---
--- Dumping routines for database 'restaurantdb'
---
-/*!50003 DROP PROCEDURE IF EXISTS `check_inventory_thresholds` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb3 */ ;
-/*!50003 SET character_set_results = utf8mb3 */ ;
-/*!50003 SET collation_connection  = utf8mb3_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `check_inventory_thresholds`()
-BEGIN
-  DECLARE done INT DEFAULT FALSE;
-  DECLARE v_ingredient_id INT UNSIGNED;
-  DECLARE v_name VARCHAR(50);
-  DECLARE v_amount INT UNSIGNED;
-  DECLARE v_restock_threshold INT UNSIGNED;
-  DECLARE v_restock_amount INT UNSIGNED;
-  
-  -- Cursor to select ingredients below threshold
-  DECLARE cur CURSOR FOR 
-    SELECT ingredient_id, name, amount, restock_threshold, restock_amount 
-    FROM inventory 
-    WHERE amount <= restock_threshold;
-  
-  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-  
-  OPEN cur;
-  
-  read_loop: LOOP
-    FETCH cur INTO v_ingredient_id, v_name, v_amount, v_restock_threshold, v_restock_amount;
-    IF done THEN
-      LEAVE read_loop;
-    END IF;
-    
-    -- Restock the ingredient
-    UPDATE inventory 
-    SET amount = amount + v_restock_amount 
-    WHERE ingredient_id = v_ingredient_id;
-    
-    -- Log the restock action
-    INSERT INTO inventory_logs (ingredient_id, action_type, quantity_change) 
-    VALUES (v_ingredient_id, 'restock', v_restock_amount);
-    
-  END LOOP;
-  
-  CLOSE cur;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `manually_order_ingredient` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb3 */ ;
-/*!50003 SET character_set_results = utf8mb3 */ ;
-/*!50003 SET collation_connection  = utf8mb3_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `manually_order_ingredient`(IN p_ingredient_id INT UNSIGNED, IN p_quantity INT UNSIGNED)
-BEGIN
-  -- Update inventory with ordered amount
-  UPDATE inventory 
-  SET amount = amount + p_quantity 
-  WHERE ingredient_id = p_ingredient_id;
-  
-  -- Log the manual order
-  INSERT INTO inventory_logs (ingredient_id, action_type, quantity_change) 
-  VALUES (p_ingredient_id, 'manual_order', p_quantity);
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -393,4 +326,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-30  0:22:42
+-- Dump completed on 2024-09-30 16:46:51
