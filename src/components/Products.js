d074f24a9d0fd86022b55d7c4ef15a609a9f4fff
ck011604 <ck011604@gmail.com>
2024-10-13 16:00:05 -0500

    add menu item modal

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,6 +6,12 @@ import '../css/Products.css';
 const Products = () => {
     const [items, setItems] = useState([]);
     const [productFilter, setProductFilter] = useState("Menu Items");
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal)
+    };
+    
     useEffect(() => {
         const fetchAllItems = async ()=>{
             try{
@@ -55,36 +61,49 @@ const Products = () => {
                 <button onClick={() => setProductFilter("REWARDS")}>Rewards</button>
                 <button onClick={() => setProductFilter("EVENTS")}>Events</button>
             </div>
-            <h2>List of Menu Items</h2>
-            
-            <table class = "menu_items_table">
-                <thead> 
-                    <tr class = "item_info">
-                        <th>Item Number</th>
-                        <th>Name</th>
-                        <th>Ingredients</th>
-                        <th>Type</th>
-                        <th>Price</th>
-                        <th>IsActive</th>
-                    </tr>
-                </thead>
-                <tbody id = "items_table">
-                    {items.map((item, key) => {
-                        return (   
-                            <tr>
-                                <td>{key}</td>
-                                <td>{item.name}</td>
-                                <td>{item.ingredients.map((ingredient, _) => {
-                                    return ( <Ingredient ingredient_id={ingredient.ingredient_id} />)
-                                })}</td>
-                                <td>{item.type}</td>
-                                <td>{item.price}</td>
-                                <td>{item.isActive}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <div>
+                <div className="add-menu-item">
+                    <h2 style={{display: "inline"}} >List of Menu Items</h2>
+                    <button onClick = {toggleModal} className="btn-modal"> + </button>
+                    {modal && (
+                        <div className="modal">
+                            <div onClick={() => toggleModal()} className="overlay"></div>
+                            <div className="modal-content">
+                                <h2>Add Item Form</h2>
+                                <button className="close-modal" onClick={() => toggleModal()}>Close</button>
+                            </div>
+                        </div>
+                    )}
+                </div>
+                <table class = "menu_items_table">
+                    <thead> 
+                        <tr class = "item_info">
+                            <th>Item Number</th>
+                            <th>Name</th>
+                            <th>Ingredients</th>
+                            <th>Type</th>
+                            <th>Price</th>
+                            <th>IsActive</th>
+                        </tr>
+                    </thead>
+                    <tbody id = "items_table">
+                        {items.map((item, key) => {
+                            return (   
+                                <tr>
+                                    <td>{key}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.ingredients.map((ingredient, _) => {
+                                        return ( <Ingredient ingredient_id={ingredient.ingredient_id} />)
+                                    })}</td>
+                                    <td>{item.type}</td>
+                                    <td>{item.price}</td>
+                                    <td>{item.isActive}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
