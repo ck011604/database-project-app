02026ea39e8c0fa89427e9bfa7e0a00c86bd9ed7
ck011604 <ck011604@gmail.com>
2024-10-16 01:32:54 -0500

    finished reusable form

--- src/components/EditForms/AddMenuItemForm.js
+++ src/components/EditForms/AddMenuItemForm.js
@@ -1,10 +1,81 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import React from "react";
 import Form from "../Reusable/Form";
+import Select from "react-select";
 
-function AddMenuItemForm(props) {
+const AddMenuItemForm = (props) => {
     const onSubmit = (values) => {
         console.log(values);
     };
+    const [ingredientsOptions, setIngredientsOptions] = useState([]);
+    const [typeOptions, setTypeOptions] = useState([]);
+
+    // useEffect(() => {
+    //     const fetchIngredients = async () => {
+    //         try {
+    //             let res = await axios.get("http://localhost:3001/api/inventory")
+    //             let ingredientsOptions = res.data.inventory;
+    //             let new_ingredients = []
+    //             for (let ingredient of ingredientsOptions) {
+    //                 new_ingredients.push({
+    //                     value: ingredient.ingredient_id,
+    //                     label: ingredient.name,
+    //                 })
+    //             }
+    //             console.log(new_ingredients)
+    //             setIngredientsOptions(new_ingredients);
+    //         } catch (err) {
+    //             console.log(`Error fetching ingredients: ${err}`);
+    //         }
+    //     };
+    //     fetchIngredients();
+    // }, []);
+
+    useEffect(() => {
+        const fetchAllItems = async ()=>{
+            try{
+                let res = await axios.get("http://localhost:3001/menu")
+                let typeOptions = res.data.menu;
+                let uniqueTypes = new Set();
+                let new_items = [];
+                for (let item of typeOptions) {
+                    if (!uniqueTypes.has(item.type)) {
+                        uniqueTypes.add(item.type);
+                        new_items.push({
+                            value: item.type,
+                            label: item.type,
+                        });
+                    }
+                }
+                console.log(new_items)
+                setTypeOptions(new_items);
+            } catch (err) {
+                console.log(`Error fetching types: ${err}`)
+            }
+        }
+        fetchAllItems()
+    }, [])
+
+    const type = [
+        { value: 'main', label: 'Main Course' },
+        { value: 'side', label: 'Side Dish' },
+        { value: 'drink', label: 'Drink' },
+    ];
+
+    // const validate = (watchValues, errorMethods) => {
+    //     const { errors, setError, clearErrors } = errorMethods;
+    
+    //     // clearErrors('itemName');
+    
+    //     // itemName validation
+    //     if(watchValues['itemName'] === 'pizza') {
+    //         setError('itemName', {
+    //             type: 'manual',
+    //             message: 'Item already exists'
+    //         });
+    //     }
+    // }
 
     const template =  {
         title: 'Add New Menu Item',
@@ -13,14 +84,55 @@ function AddMenuItemForm(props) {
             {
                 title: 'Item Name',
                 type: 'text',
-                name: 'itemName'
-            }
+                name: 'itemName',
+                validationProps: {
+                    required: 'Item name is mandatory'
+                }
+            },
+            {
+                title: 'Ingredients',
+                name: 'ingredients',
+                type: 'select',
+                rules: { required: true },
+                render: ({ field }) => (<Select {...field} isMulti options={ingredientsOptions} />),
+                validationProps: {
+                    required: 'Ingredients is mandatory'
+                }
+            },
+            {
+                title: 'Price',
+                type: 'number',
+                name: 'price',
+                validationProps: {
+                    required: 'Price is mandatory'
+                }
+            },
+            {
+                title: 'Image',
+                type: 'text',
+                name: 'image',
+                // validationProps: {
+                //     required: 'Image is mandatory'
+                // }
+            },
+            {
+                title: 'Type',
+                name: 'type',
+                type: 'select',
+                rules: { required: true },
+                render: ({ field }) => (<Select {...field} options={typeOptions} />),
+                validationProps: {
+                    required: 'Item type is mandatory'
+                }
+            },
         ]
     }
 
     return (
         <Form
             template={template}
+            watchFields={['itemName']}
+            // validate={validate}
             // Allow access to data within component
             onSubmit={onSubmit}
         />
--- src/components/Reusable/Form.js
+++ src/components/Reusable/Form.js
@@ -1,23 +1,70 @@
-import React from "react";
-import { useForm } from 'react-hook-form';
+import React, {useEffect} from "react";
+import { useForm, Controller } from 'react-hook-form';
+import Select from "react-select";
 
 // Reusable Form Component
-function Form({ template, onSubmit }) {
+function Form({ template, onSubmit, watchFields, validate }) {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors }, watch, setError, clearErrors, control } = useForm({
+        // defaultValues: {
+        //     itemName: '',
+        //     ID: ''
+        // }
+    });
+    
     // Parameters needed to create Form
     const { title, fields } = template;
+
+    const watchValues = watch();
+    console.log(watchValues);
+    
+    // useEffect(() => {
+    //     validate(watchValues, { errors, setError, clearErrors });
+    //     // console.log(watchValues);
+    // }, [watchValues]);
 
     // Render every data type in fields
     const renderFields = (fields) => {
         return fields.map (field => {
-            const { title, type, name } = field;
-            return(
-                <div key={name}>
-                    <label htmlFor={name}>{title}</label>
-                    <input type='text' name={name} id={name} {...register('${name}')}></input>
-                </div>
-            )
+            const { title, type, name, validationProps, rules, render } = field;
+
+            switch(type){
+                case 'text':
+                    return(
+                        <div key={name}>
+                            <label htmlFor={name}>{title}</label>
+                            <input type='text' name={name} id={name} {...register(name, validationProps)}></input>
+                            { errors[name] && <span className='red-text'>{errors[name]['message']}</span>}
+                        </div>
+                    )
+                case 'number':
+                    return(
+                        <div key={name}>
+                            <label htmlFor={name}>{title}</label>
+                            <input type='number' name={name} id={name} {...register(name, validationProps)}></input>
+                            { errors[name] && <span className='red-text'>{errors[name]['message']}</span>}
+                        </div>
+                    )
+                case 'select':
+                    return (
+                        <div key={name}>
+                            <label htmlFor={name}>{title}</label>
+                            <Controller
+                                name={name}
+                                control={control}
+                                rules={rules}
+                                render={render}
+                            />
+                            {errors[name] && <span className='red-text'>{errors[name]['message']}</span>}
+                        </div>
+                    );
+                default:
+                    return(
+                        <div key={name}>
+                            <span className='red-text'>Invalid Field</span>
+                        </div>
+                    )
+            }
         })
     }
 
