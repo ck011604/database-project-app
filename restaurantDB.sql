489bd5c4d738e1621054f5fed2987f0948b459cc
Mauricio <133822606+mauriciolemusr@users.noreply.github.com>
2024-10-21 22:23:46 -0500

    Add files via upload

--- restaurantDB.sql
+++ restaurantDB.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 9.0.1, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: restaurantdb
 -- ------------------------------------------------------
@@ -88,7 +88,7 @@ CREATE TABLE `inventory` (
 --
 
 /*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
-INSERT INTO `inventory` VALUES (1,'mozzarella',75,20,100),(2,'tomato sauce',95,10,100),(3,'dough',95,10,50),(4,'pepperoni',100,50,100),(5,'cheddar cheese',100,20,100),(6,'beef patty',100,20,50),(7,'lettuce',100,20,50),(8,'tomato',100,20,50),(9,'pasta',100,20,50),(10,'chicken',100,20,70),(11,'garlic',100,20,50),(12,'fish fillets',100,10,30),(13,'tortillas',100,20,50),(14,'cabbage',100,20,50),(15,'lime',100,40,50),(16,'sour cream',100,20,50),(17,'milk',100,20,50),(18,'butter',50,30,100),(19,'flour',100,40,200),(20,'hamburger bun',100,20,75),(21,'hot dog sausage',100,20,50),(22,'hot dog bun',100,30,100),(23,'mustard',100,40,100),(24,'ketchup',100,30,100),(25,'relish',100,20,45),(26,'potatoes',100,30,100),(27,'carrots',100,20,60),(28,'mayonnaise',100,10,40),(29,'bread',100,10,40),(30,'corn',80,10,50),(31,'salt',70,40,100),(32,'Sprite',58,20,100),(33,'Coca-Cola',65,20,100),(34,'Pepsi',100,20,100),(35,'Dr. Pepper',100,20,100),(36,'Fanta Orange',0,20,100),(37,'Diet Coke',95,20,100),(38,'Black Tea',100,20,70),(39,'Sugar',100,20,100);
+INSERT INTO `inventory` VALUES (1,'mozzarella',115,20,100),(2,'tomato sauce',90,10,100),(3,'dough',90,10,50),(4,'pepperoni',100,50,100),(5,'cheddar cheese',100,20,100),(6,'beef patty',100,20,50),(7,'lettuce',94,20,50),(8,'tomato',100,20,50),(9,'pasta',100,20,50),(10,'chicken',100,20,70),(11,'garlic',100,20,50),(12,'fish fillets',100,10,30),(13,'tortillas',100,20,50),(14,'cabbage',100,20,50),(15,'lime',100,40,50),(16,'sour cream',100,20,50),(17,'milk',100,20,50),(18,'butter',50,30,100),(19,'flour',100,40,200),(20,'hamburger bun',100,20,75),(21,'hot dog sausage',100,20,50),(22,'hot dog bun',100,30,100),(23,'mustard',100,40,100),(24,'ketchup',100,30,100),(25,'relish',100,20,45),(26,'potatoes',100,30,100),(27,'carrots',100,20,60),(28,'mayonnaise',100,10,40),(29,'bread',100,10,40),(30,'corn',80,10,50),(31,'salt',70,40,100),(32,'Sprite',58,20,100),(33,'Coca-Cola',65,20,100),(34,'Pepsi',100,20,100),(35,'Dr. Pepper',100,20,100),(36,'Fanta Orange',0,20,100),(37,'Diet Coke',95,20,100),(38,'Black Tea',99,20,70),(39,'Sugar',95,20,100);
 /*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -103,24 +103,38 @@ INSERT INTO `inventory` VALUES (1,'mozzarella',75,20,100),(2,'tomato sauce',95,1
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
-DROP TABLE IF EXISTS inventory_summary_logs
+--
+-- Table structure for table `inventory_logs`
+--
 
-CREATE TABLE inventory_summary_logs (
-    log_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    ingredient_id INT UNSIGNED NOT NULL,
-    starting_amount INT UNSIGNED NOT NULL,
-    ending_amount INT UNSIGNED NOT NULL,
-    restocked_amount INT UNSIGNED NOT NULL DEFAULT 0,
-    used_amount INT UNSIGNED NOT NULL DEFAULT 0,
-    action_type ENUM('restock', 'usage', 'manual_adjustment') NOT NULL,
-    quantity_change INT NOT NULL,
-    PRIMARY KEY (log_id),
-    INDEX idx_timestamp_ingredient (timestamp, ingredient_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `inventory_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory_logs` (
+  `log_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `ingredient_id` int unsigned NOT NULL,
+  `action_type` enum('restock','used','discarded') NOT NULL,
+  `quantity_change` int NOT NULL,
+  `log_date` date NOT NULL DEFAULT (curdate()),
+  `log_time` time NOT NULL DEFAULT (curtime()),
+  PRIMARY KEY (`log_id`),
+  KEY `idx_ingredient_date` (`ingredient_id`,`log_date`),
+  CONSTRAINT `fk_inventory_logs_ingredient_id` FOREIGN KEY (`ingredient_id`) REFERENCES `inventory` (`ingredient_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='logs for inventory changes';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory_logs`
+--
+
+/*!40000 ALTER TABLE `inventory_logs` DISABLE KEYS */;
+INSERT INTO `inventory_logs` VALUES (1,1,'used',-10,'2024-10-21','21:16:56'),(2,2,'used',-5,'2024-10-21','21:16:56'),(3,3,'used',-5,'2024-10-21','21:16:56'),(4,7,'discarded',-2,'2024-10-21','21:17:42'),(5,8,'discarded',-3,'2024-10-21','21:17:42'),(6,1,'restock',25,'2024-10-21','21:18:15'),(7,1,'used',-10,'2024-10-21','21:25:45'),(8,1,'used',-10,'2024-10-21','21:26:08'),(9,1,'used',-10,'2024-10-21','21:26:12'),(10,1,'used',-10,'2024-10-21','21:26:18'),(11,1,'used',-10,'2024-10-21','21:26:20'),(12,1,'restock',100,'2024-10-21','21:26:20'),(13,38,'used',-1,'2024-10-21','21:38:53'),(14,39,'used',-5,'2024-10-21','21:38:53');
+/*!40000 ALTER TABLE `inventory_logs` ENABLE KEYS */;
+
 --
 -- Table structure for table `menu`
 --
+
 DROP TABLE IF EXISTS `menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -178,7 +192,7 @@ CREATE TABLE `orders` (
   CONSTRAINT `fk_orders_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_orders_promoCode_id` FOREIGN KEY (`promoCode_id`) REFERENCES `promotion_codes` (`promoCode_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_orders_waiter_id` FOREIGN KEY (`waiter_id`) REFERENCES `employees` (`employee_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='receipt and payment info';
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='receipt and payment info';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -186,7 +200,7 @@ CREATE TABLE `orders` (
 --
 
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,'[{\"name\": \"Cheese Pizza\", \"type\": \"main\", \"image\": \"cheese_pizza.jpg\", \"price\": \"15.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 1, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 2}, {\"quantity\": 5, \"ingredient_id\": 1}, {\"quantity\": 1, \"ingredient_id\": 3}]}]',1,NULL,79.95,0,0.00,86.55,90.00,3.45,6.60,'2024-10-17 04:50:02','',NULL,5,79,0,0),(2,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,1,9.95,15,1.49,12.26,15.00,2.74,0.82,'2024-10-17 04:51:46','',NULL,1,9,0,0),(3,'[{\"name\": \"Coca-Cola\", \"type\": \"drink\", \"image\": \"coca-cola.png\", \"price\": \"1.99\", \"quantity\": 10, \"is_active\": 1, \"recipe_id\": 13, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 33}]}]',1,1,19.90,5,0.80,18.03,20.00,1.97,1.31,'2024-10-17 13:28:20','',1,21,19,1,20),(4,'[{\"name\": \"Coca-Cola\", \"type\": \"drink\", \"image\": \"coca-cola.png\", \"price\": \"1.99\", \"quantity\": 15, \"is_active\": 1, \"recipe_id\": 13, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 33}]}]',1,1,29.85,0,0.00,25.85,30.00,4.15,1.97,'2024-10-17 13:31:01','',1,22,29,1,20),(5,'[{\"name\": \"Corn\", \"type\": \"side\", \"image\": \"corn.jpg\", \"price\": \"3.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 11, \"ingredients\": [{\"quantity\": 2, \"ingredient_id\": 30}, {\"quantity\": 3, \"ingredient_id\": 31}, {\"quantity\": 5, \"ingredient_id\": 18}]}]',1,NULL,19.95,0,0.00,19.44,20.00,0.56,1.48,'2024-10-17 14:50:15','',NULL,3,19,1,10),(6,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,9.95,5,0.45,10.14,15.00,4.86,0.74,'2024-10-17 15:34:03','',NULL,5,9,1,10),(7,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 10, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,1,19.90,10,1.59,18.83,20.00,1.17,1.31,'2024-10-17 15:38:17','',1,30,19,1,20),(8,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 10, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,19.90,0,0.00,17.23,20.00,2.77,1.31,'2024-10-17 15:43:40','',1,22,19,1,20),(9,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,9.95,15,1.19,9.81,10.00,0.19,0.66,'2024-10-17 15:52:53','',1,11,9,1,20),(10,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,1.99,0,0.00,1.72,2.00,0.28,0.13,'2024-10-17 15:54:54','',1,23,1,1,20),(11,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,1.99,0,0.00,1.72,2.00,0.28,0.13,'2024-10-17 15:56:26','',1,1,1,1,20),(12,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,1.99,0,0.00,1.72,2.00,0.28,0.13,'2024-10-17 16:00:25','',1,1,1,1,20),(13,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,1.99,0,0.00,1.72,2.00,0.28,0.13,'2024-10-17 16:02:16','',1,1,1,1,20),(14,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 3, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,5.97,0,0.00,5.17,6.00,0.83,0.39,'2024-10-17 16:08:42','',1,3,5,1,20),(15,'[{\"name\": \"Corn\", \"type\": \"side\", \"image\": \"corn.jpg\", \"price\": \"3.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 11, \"ingredients\": [{\"quantity\": 2, \"ingredient_id\": 30}, {\"quantity\": 3, \"ingredient_id\": 31}, {\"quantity\": 5, \"ingredient_id\": 18}]}]',1,1,19.95,10,1.80,21.23,25.00,3.77,1.48,'2024-10-17 16:12:38','',NULL,5,19,1,10),(16,'[{\"name\": \"Coca-Cola\", \"type\": \"drink\", \"image\": \"coca-cola.png\", \"price\": \"1.99\", \"quantity\": 10, \"is_active\": 1, \"recipe_id\": 13, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 33}]}]',1,NULL,19.90,0,0.00,17.23,20.00,2.77,1.31,'2024-10-17 19:26:00','',1,5,19,1,20),(17,'[{\"name\": \"Diet Coke\", \"type\": \"drink\", \"image\": \"diet_coke.png\", \"price\": \"1.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 17, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 37}]}]',1,NULL,9.95,0,0.00,9.59,10.00,0.41,0.73,'2024-10-17 19:29:23','',2,5,9,1,11);
+INSERT INTO `orders` VALUES (1,'[{\"name\": \"Cheese Pizza\", \"type\": \"main\", \"image\": \"cheese_pizza.jpg\", \"price\": \"15.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 1, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 2}, {\"quantity\": 5, \"ingredient_id\": 1}, {\"quantity\": 1, \"ingredient_id\": 3}]}]',1,NULL,79.95,0,0.00,86.55,90.00,3.45,6.60,'2024-10-17 04:50:02','',NULL,5,79,0,0),(2,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,1,9.95,15,1.49,12.26,15.00,2.74,0.82,'2024-10-17 04:51:46','',NULL,1,9,0,0),(3,'[{\"name\": \"Coca-Cola\", \"type\": \"drink\", \"image\": \"coca-cola.png\", \"price\": \"1.99\", \"quantity\": 10, \"is_active\": 1, \"recipe_id\": 13, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 33}]}]',1,1,19.90,5,0.80,18.03,20.00,1.97,1.31,'2024-10-17 13:28:20','',1,21,19,1,20),(4,'[{\"name\": \"Coca-Cola\", \"type\": \"drink\", \"image\": \"coca-cola.png\", \"price\": \"1.99\", \"quantity\": 15, \"is_active\": 1, \"recipe_id\": 13, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 33}]}]',1,1,29.85,0,0.00,25.85,30.00,4.15,1.97,'2024-10-17 13:31:01','',1,22,29,1,20),(5,'[{\"name\": \"Corn\", \"type\": \"side\", \"image\": \"corn.jpg\", \"price\": \"3.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 11, \"ingredients\": [{\"quantity\": 2, \"ingredient_id\": 30}, {\"quantity\": 3, \"ingredient_id\": 31}, {\"quantity\": 5, \"ingredient_id\": 18}]}]',1,NULL,19.95,0,0.00,19.44,20.00,0.56,1.48,'2024-10-17 14:50:15','',NULL,3,19,1,10),(6,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,9.95,5,0.45,10.14,15.00,4.86,0.74,'2024-10-17 15:34:03','',NULL,5,9,1,10),(7,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 10, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,1,19.90,10,1.59,18.83,20.00,1.17,1.31,'2024-10-17 15:38:17','',1,30,19,1,20),(8,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 10, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,19.90,0,0.00,17.23,20.00,2.77,1.31,'2024-10-17 15:43:40','',1,22,19,1,20),(9,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,9.95,15,1.19,9.81,10.00,0.19,0.66,'2024-10-17 15:52:53','',1,11,9,1,20),(10,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,1.99,0,0.00,1.72,2.00,0.28,0.13,'2024-10-17 15:54:54','',1,23,1,1,20),(11,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,1.99,0,0.00,1.72,2.00,0.28,0.13,'2024-10-17 15:56:26','',1,1,1,1,20),(12,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,1.99,0,0.00,1.72,2.00,0.28,0.13,'2024-10-17 16:00:25','',1,1,1,1,20),(13,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,1.99,0,0.00,1.72,2.00,0.28,0.13,'2024-10-17 16:02:16','',1,1,1,1,20),(14,'[{\"name\": \"Sprite\", \"type\": \"drink\", \"image\": \"sprite.png\", \"price\": \"1.99\", \"quantity\": 3, \"is_active\": 1, \"recipe_id\": 12, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 32}]}]',1,NULL,5.97,0,0.00,5.17,6.00,0.83,0.39,'2024-10-17 16:08:42','',1,3,5,1,20),(15,'[{\"name\": \"Corn\", \"type\": \"side\", \"image\": \"corn.jpg\", \"price\": \"3.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 11, \"ingredients\": [{\"quantity\": 2, \"ingredient_id\": 30}, {\"quantity\": 3, \"ingredient_id\": 31}, {\"quantity\": 5, \"ingredient_id\": 18}]}]',1,1,19.95,10,1.80,21.23,25.00,3.77,1.48,'2024-10-17 16:12:38','',NULL,5,19,1,10),(16,'[{\"name\": \"Coca-Cola\", \"type\": \"drink\", \"image\": \"coca-cola.png\", \"price\": \"1.99\", \"quantity\": 10, \"is_active\": 1, \"recipe_id\": 13, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 33}]}]',1,NULL,19.90,0,0.00,17.23,20.00,2.77,1.31,'2024-10-17 19:26:00','',1,5,19,1,20),(17,'[{\"name\": \"Diet Coke\", \"type\": \"drink\", \"image\": \"diet_coke.png\", \"price\": \"1.99\", \"quantity\": 5, \"is_active\": 1, \"recipe_id\": 17, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 37}]}]',1,NULL,9.95,0,0.00,9.59,10.00,0.41,0.73,'2024-10-17 19:29:23','',2,5,9,1,11),(18,'[{\"name\": \"Sweet Tea\", \"type\": \"drink\", \"image\": \"sweet_tea.jpg\", \"price\": \"1.99\", \"quantity\": 1, \"is_active\": 1, \"recipe_id\": 18, \"ingredients\": [{\"quantity\": 1, \"ingredient_id\": 38}, {\"quantity\": 5, \"ingredient_id\": 39}]}]',1,NULL,1.99,0,0.00,2.15,2.15,0.00,0.16,'2024-10-22 02:38:53','',NULL,1,1,1,0);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 
 --
@@ -323,19 +337,6 @@ INSERT INTO `users` VALUES (1,'Mike','Ross','mikeross@gmail.com','mike123','user
 --
 -- Dumping routines for database 'restaurantdb'
 --
-/*!50003 DROP PROCEDURE IF EXISTS `generate_daily_inventory_summary` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'IGNORE_SPACE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -346,4 +347,4 @@ INSERT INTO `users` VALUES (1,'Mike','Ross','mikeross@gmail.com','mike123','user
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-21 19:30:58
+-- Dump completed on 2024-10-21 22:00:41
