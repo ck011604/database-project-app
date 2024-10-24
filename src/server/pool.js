c45b720012d02e6cfc3f997a0afee3a48ca8d0b1
Mauricio <m.lemus@outlook.com>
2024-10-24 18:10:45 -0500

    Changed pool file to custom settings

--- src/server/pool.js
+++ src/server/pool.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',   //  database host
     user: 'root', //  MySQL username
     password: 'yuca69!', //  MySQL password
-    database: 'restaurant_db', // database name
+    database: 'restaurantDB', // database name
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
