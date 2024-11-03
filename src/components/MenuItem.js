a6f5fae836874ada9b9edefb102f219ca4a55949
Canly <ck011604@gmail.com>
2024-11-03 09:34:30 -0600

    fixed image upload

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -7,7 +7,7 @@ const MenuItem = ({ item, onSelect, className }) => {
 
   return (
     <div className={`item-box ${className}`} onClick={() => onSelect(item)}>
-      <img src={`/menu_images/${item.image}`} alt={item.name} className="item-image" />
+      <img src={`${process.env.REACT_APP_API_URL}/static/menu_images/${item.image}`} alt={item.name} className="item-image" />
       <div className="item-name"> {item.name} </div>
       <div className="item-price"> ${item.price} </div>
     </div>
