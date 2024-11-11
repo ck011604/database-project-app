8095f021d18c79ea013fc58bada5c8eb7f889831
Andrew <andrewqp03@gmail.com>
2024-11-10 20:53:23 -0600

    Updated CSS with animation

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,7 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import "../css/NotFoundPage.css"
 const NotFoundPage = () => {
-    return ( <div className="not-found-page">
-        <h1>Page not found</h1>
-    </div> );
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        window.history.go(-2);
+    };
+    return ( 
+        <div className="not-found-page">
+            <div className="not-found-page-circle">
+                <p className="page-not-found-text">OPPS! PAGE NOT FOUND</p>
+                <h1 className="code-404">404</h1>
+                <p className="page-not-found-text">WE ARE SORRY, BUT THE PAGE YOUR REQUESTED WAS NOT FOUND</p>
+                <button className="page-not-found-back-button" onClick={handleGoBack}>Go Back</button>
+            </div>
+        </div> 
+    );
 }
  
 export default NotFoundPage;
