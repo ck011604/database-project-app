bfb05120ca094ca25ca1663d935dc840973de4f5
Bade1 <bade15004@gmail.com>
2024-11-15 22:38:46 -0600

    Added more apis for Order Report

--- server/controllers/orders_report_controller.js
+++ server/controllers/orders_report_controller.js
@@ -69,5 +69,143 @@ module.exports = {
                 orderData: results
             }));
         });
+    },
+
+    getOrdersByHours: (req, res) =>{
+        const parsedUrl = url.parse(req.url, true);
+        const {startDate, endDate} = parsedUrl.query;
+        let dateFilter = '';
+        let params = [];
+
+        if(startDate && endDate){
+            console.log(`Start Date: ${startDate} and End Date: ${endDate}`);
+            dateFilter = 'WHERE DATE(time) BETWEEN ? AND ?';
+            params = [
+                startDate,
+                endDate
+            ];
+        }
+
+        const query = `
+            SELECT HOUR(time) AS hour, COUNT(*) AS order_count
+            FROM orders
+            ${dateFilter}
+            GROUP BY hour
+            ORDER BY hour;
+        `;
+
+        pool.query(query, params, (error, results) => {
+            if (error) {
+                console.error('Error in report query:', error);
+                res.writeHead(500, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ 
+                    success: false, 
+                    message: "Server Error fetching customer orders hours data"
+                }));
+                console.log(`Error fetching orders ${error}`);
+                return;
+            }
+            console.log("Successfully fetched orders by hours");
+            
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ 
+                success: true, 
+                orderDataHours: results
+            }));
+        });
+
+    },
+
+    getOrdersByDays: (req, res) =>{
+        const parsedUrl = url.parse(req.url, true);
+        const {startDate, endDate} = parsedUrl.query;
+        let dateFilter = '';
+        let params = [];
+
+        if(startDate && endDate){
+            console.log(`Start Date: ${startDate} and End Date: ${endDate}`);
+            dateFilter = 'WHERE DATE(time) BETWEEN ? AND ?';
+            params = [
+                startDate,
+                endDate
+            ];
+        }
+
+        const query = `
+            SELECT DAYNAME(time) AS day, COUNT(*) AS order_count
+            FROM orders
+            ${dateFilter}
+            GROUP BY day
+            ORDER BY day;
+        `;
+
+        pool.query(query, params, (error, results) => {
+            if (error) {
+                console.error('Error in report query:', error);
+                res.writeHead(500, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ 
+                    success: false, 
+                    message: "Server Error fetching customer orders days data"
+                }));
+                console.log(`Error fetching orders ${error}`);
+                return;
+            }
+            console.log("Successfully fetched orders by days");
+            
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ 
+                success: true, 
+                orderDataDays: results
+            }));
+        });
+
+    },
+
+    getOrdersByMonths: (req, res) =>{
+        const parsedUrl = url.parse(req.url, true);
+        const {startDate, endDate} = parsedUrl.query;
+        let dateFilter = '';
+        let params = [];
+
+        if(startDate && endDate){
+            console.log(`Start Date: ${startDate} and End Date: ${endDate}`);
+            dateFilter = 'WHERE DATE(time) BETWEEN ? AND ?';
+            params = [
+                startDate,
+                endDate
+            ];
+        }
+        
+        const query = `
+            SELECT MONTHNAME(time) AS month, COUNT(*) AS order_count
+            FROM orders
+            ${dateFilter}
+            GROUP BY month
+            ORDER BY month;
+        `;
+
+        pool.query(query, params, (error, results) => {
+            if (error) {
+                console.error('Error in report query:', error);
+                res.writeHead(500, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ 
+                    success: false, 
+                    message: "Server Error fetching customer orders months data"
+                }));
+                console.log(`Error fetching orders ${error}`);
+                return;
+            }
+            console.log("Successfully fetched orders by months");
+            
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ 
+                success: true, 
+                orderDataMonths: results
+            }));
+        });
+
     }
+
+
+    
 }
